a82d2c76ed42bc44c374372c3a14adbfe5d95120
Vaishakh Kallattil <vkallattil@gmail.com>
2025-02-04 18:18:47 -0500

    working sync

--- src/handlers.ts
+++ src/handlers.ts
@@ -2,28 +2,36 @@ import type { WSContext, WSMessageReceive } from "hono/ws";
 import { v4 as uuidv4 } from "uuid";
 
 const activeConnections = new Map<string, WSContext<unknown>>();
+const connectionIds = new Map<WSContext<unknown>, string>();
 
 export const onMessageHandler = (
   event: MessageEvent<WSMessageReceive>,
   ws: WSContext<unknown>
-) => {
-  console.log(`Message from client: ${event.data}`);
-  ws.send(`${event.data}`);
+) => {  
+  const connectionId = connectionIds.get(ws);
+  console.log(`Message from client ${connectionId}: ${event.data}`);
+  for (const [id, ws] of activeConnections) {
+    console.log(`Sending message to client ${id}`);
+    ws.send(`${event.data}`);
+  }
 };
 
 export const onOpenHandler = (event: Event, ws: WSContext<unknown>) => {
   console.log("Connection opened");
   const id = uuidv4();
-  (ws as any).connectionId = id;
+  connectionIds.set(ws, id);
   activeConnections.set(id, ws);
+  console.log([...activeConnections.keys()]);
 };
 
 export const onCloseHandler = (event: Event, ws: WSContext<unknown>) => {
-  const connectionId = (ws as any).connectionId;
+  const connectionId = connectionIds.get(ws);
   if (connectionId) {
     activeConnections.delete(connectionId);
+    connectionIds.delete(ws);
     console.log(
       `Connection ${connectionId} closed. Total: ${activeConnections.size}`
     );
   }
+  console.log([...activeConnections.keys()]);
 };
